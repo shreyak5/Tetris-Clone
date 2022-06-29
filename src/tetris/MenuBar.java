5ae51cfcdfceb3c4fcf8d0b5897b0e4a552c8ba1
shreyak5 <84128385+shreyak5@users.noreply.github.com>
2022-06-30 01:29:36 +0530

    added menu items to the menus

--- src/tetris/MenuBar.java
+++ src/tetris/MenuBar.java
@@ -16,6 +16,21 @@ class MenuBar extends JMenuBar
         game = new JMenu("Game");
         help = new JMenu("Help");
 
+        //Menu items for Game menu
+        newGame = new JMenuItem("New game");
+        pause = new JMenuItem("Pause");
+        resume = new JMenuItem("Resume");
+        exit = new JMenuItem("Exit");
+        
+        game.add(newGame);
+        game.add(pause);
+        game.add(resume);
+        game.add(exit);
+
+        //Menu item for Help menu
+        instructions = new JMenuItem("Instructions");
+        help.add(instructions);
+
         //Adding the menus to the menubar
         add(game);
         add(help);

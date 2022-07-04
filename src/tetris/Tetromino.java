57cb6c55f43e8f9c099db33c81c7655815865190
shreyak5 <84128385+shreyak5@users.noreply.github.com>
2022-07-05 00:20:44 +0530

    created initializer functions for all shapes

--- src/tetris/Tetromino.java
+++ src/tetris/Tetromino.java
@@ -1,8 +1,8 @@
 package tetris;
 
 import java.awt.*;
-import javax.swing.*;
 import java.util.Hashtable;
+import java.util.Random;
 
 class Tetromino 
 {
@@ -19,6 +19,7 @@ class Tetromino
         Colours.put(Shape.Z_shape, Color.ORANGE);
         Colours.put(Shape.T_shape, Color.MAGENTA);
     }
+    Random rand = new Random();
 
     //Tetromino instance variables
     Shape piece;
@@ -26,7 +27,7 @@ class Tetromino
     int curr_orientation;
     int possible_orientations[][][];
     int currPos[];
-    int maxLeft, maxRight, maxDown;
+    // int maxLeft, maxRight, maxDown;   
 
     //Constructor
     Tetromino(Shape shape)
@@ -64,38 +65,73 @@ class Tetromino
     //Functions used to initialize different tetromino shapes
     void initOshape()
     {
-
+        possible_orientations = new int[][][]{{{0, 0}, {0, 1},{1, 0},{1, 1}}};
+        curr_orientation = 0;
     }
 
     void initIshape()
     {
-
+        possible_orientations = new int[][][]{
+            {{0, -2}, {0, -1}, {0, 0}, {0, 1}},
+            {{-2, 0}, {-1, 0}, {0, 0}, {1, 0}}
+        };
+        curr_orientation = rand.nextInt(0, possible_orientations.length);
     }
 
     void initJshape()
     {
-
+        possible_orientations = new int[][][]{
+            {{0, -1}, {0, 0}, {0, 1}, {1, 1}},
+            {{1, -1}, {1, 0}, {0, 0}, {-1, 0}},
+            {{-1, -1}, {0, -1}, {0, 0}, {0, 1}},
+            {{-1, 0}, {-1, -1}, {0, 0}, {1, 0}}
+        };
+        curr_orientation = rand.nextInt(0, possible_orientations.length);
     }
 
     void initLshape()
     {
-
+        possible_orientations = new int[][][]{
+            {{1, -1}, {0, -1}, {0, 0}, {0, 1}},
+            {{-1, -1}, {-1, 0}, {0, 0}, {1, 0}},
+            {{0, -1}, {0, 0}, {0, 1}, {-1, 1}},
+            {{-1, 0}, {0, 0}, {1, 0}, {1, 1}}
+        };
+        curr_orientation = rand.nextInt(0, possible_orientations.length);
     }
 
     void initSshape()
     {
-
+        possible_orientations = new int[][][]{
+            {{1, -1}, {1, 0}, {0, 0}, {0, 1}},
+            {{-1, 0}, {0, 0}, {0, 1}, {1, 1}}
+        };
+        curr_orientation = rand.nextInt(0, possible_orientations.length);
     }
 
     void initZshape()
     {
-
+        possible_orientations = new int[][][]{
+            {{0, -1}, {0, 0}, {1, 0}, {1, 1}},
+            {{-1, 1}, {0, 1}, {0, 0}, {1, 0}},
+        };
+        curr_orientation = rand.nextInt(0, possible_orientations.length);
     }
 
     void initTshape()
     {
-
+        possible_orientations = new int[][][]{
+            {{0, -1}, {0, 0}, {0, 1}, {1, 0}},
+            {{0, -1}, {0, 0}, {-1, 0}, {1, 0}},
+            {{-1, 0}, {0, 0}, {0, -1}, {0, 1}},
+            {{-1, 0}, {0, 0}, {0, 1}, {1, 0}}
+        };
+        curr_orientation = rand.nextInt(0, possible_orientations.length);
     }
-   
+
+    // public static void main(String args[])
+    // {
+    //     new Tetromino(Shape.I_shape);
+    // }
 
 }

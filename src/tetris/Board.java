52a18d76e8143639303d56f3cce0c6f4bc7fd268
shreyak5 <84128385+shreyak5@users.noreply.github.com>
2022-07-05 20:19:56 +0530

    setup timer and downward movement of current piece

--- src/tetris/Board.java
+++ src/tetris/Board.java
@@ -7,24 +7,31 @@ import java.awt.*;
 
 class Board extends JPanel
 {
+
     private int SQUARE_SIZE;
     private Color board_colour = Color.LIGHT_GRAY;
     private Color grid_colour = Color.GRAY;
 
-    protected Color gameBoard[][];
+    protected Color game_board[][];
+
+
+
+    Tetromino curr_piece;
+    Tetromino next_piece;
 
     Board(int square_size)
     {
         SQUARE_SIZE = square_size;
         setBackground(board_colour);
 
-        //initializing gameboard
-        gameBoard = new Color[20][10];
+        //initializing game_board
+        game_board = new Color[20][10];
         for(int row = 0; row < 20; row++)
         {
             for(int column = 0; column < 10; column++)
-                gameBoard[row][column] = board_colour;
+                game_board[row][column] = board_colour;
         }
+            
     }
 
     @Override
@@ -32,6 +39,8 @@ class Board extends JPanel
     {
         super.paintComponent(g);
         paintGrid(g);
+        if(Game.game_status != Game.Status.NONE)
+            paintCurrentPiece(g);
     }
 
     void paintGrid(Graphics g)
@@ -40,7 +49,37 @@ class Board extends JPanel
         for(int i = 0; i < 20; i++)
         {
             for(int j = 0; j < 10; j++)
-                g.drawRoundRect(SQUARE_SIZE*j, SQUARE_SIZE*i, SQUARE_SIZE, SQUARE_SIZE, 5, 5);
+               drawBlock(i, j, g); 
         }
     }
+
+    void paintCurrentPiece(Graphics g)
+    {
+        int orientations[][] = curr_piece.possible_orientations[curr_piece.curr_orientation];
+        int row = curr_piece.currPos[0];
+        int column = curr_piece.currPos[1];
+
+        g.setColor(curr_piece.piece_colour);
+        //printing all blocks of the current piece
+        for(int i = 0; i < orientations.length; i++)
+        {
+            int x = orientations[i][1];
+            int y = orientations[i][0];
+            g.setColor(curr_piece.piece_colour);
+            fillBlock(row + y, column + x, g);
+            g.setColor(Color.BLACK);
+            drawBlock(row + y, column + x, g);
+        }
+    }
+
+    void drawBlock(int row, int column, Graphics g)
+    {
+        g.drawRoundRect(SQUARE_SIZE*column, SQUARE_SIZE*row, SQUARE_SIZE, SQUARE_SIZE, 5, 5);
+    }
+
+    void fillBlock(int row, int column, Graphics g)
+    {
+        g.fillRoundRect(SQUARE_SIZE*column, SQUARE_SIZE*row, SQUARE_SIZE, SQUARE_SIZE, 5, 5);
+    }
+    
 }
--- src/tetris/Game.java
+++ src/tetris/Game.java
@@ -1,9 +1,57 @@
 package tetris;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+import java.util.Random;
 
 class Game 
 {
+    static enum Status{ONGOING, PAUSED, NONE};
+    static Status game_status = Status.NONE;
+
+    private int current_score;
+    private Timer timer;
+
+    static Tetromino curr_piece;
+    static Tetromino next_piece;
+    int minY;
+    Board board;
+    Random rand = new Random();
+
+    //Constructor for new Game
+    Game(Board b)
+    {
+        board = b;
+        current_score = 0;
+        game_status = Status.ONGOING;
+        minY = 20;
+
+        //initializing timer
+        timer = new Timer(1000, new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                board.curr_piece.currPos[0]++;
+                board.repaint();
+            }
+        });
+    
+        startGame();
+    }
+
+    void startGame()
+    {
+        //Initializing curr_piece and next_piece
+        int r = rand.nextInt(0, Tetromino.Shape.values().length);
+        board.curr_piece = new Tetromino(Tetromino.Shape.values()[r]);
+
+        r = rand.nextInt(0, Tetromino.Shape.values().length);
+        board.next_piece = new Tetromino(Tetromino.Shape.values()[r]);
+
+        //initializing current position of current piece
+        board.curr_piece.currPos = new int[]{1, 4};
+
+        timer.start();
+    }
     
 }
